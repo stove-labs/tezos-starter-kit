aec3741f5ef135928205785709b3cafddad5adec
dexterslabor <dexterslabor@users.noreply.github.com>
2021-01-11 15:16:44 +0000

    Update postinstall.js

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,12 +2,12 @@ const { execSync } = require('child_process');
 const ganacheCliInstalled = require('../node_modules/ganache-cli/package.json');
 
 const baseOptions = { stdio: 'inherit' };
+// this patch does not work when reading the required ganache version from ../package.json
 const ganacheVersionRequired = '6.11.0-tezos.0';
 
 (function () {
-    console.log("ganache installed is", ganacheCliInstalled.version)
     if (ganacheCliInstalled.version !== ganacheVersionRequired) {
-        console.log('Updating ganache-cli')
+        console.log('Updating ganache-cli to ', ganacheVersionRequired);
         execSync(`npm i ganache-cli@${ganacheVersionRequired}`, baseOptions)  
     };  
 })();
