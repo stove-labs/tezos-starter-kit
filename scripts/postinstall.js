08c9681b25d86c4b983406c07c5dff973d214114
dexterslabor <dexterslabor@gmail.com>
2021-01-11 15:05:54 +0000

    feat(npm): remove hardcoded version in postinstall script

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,13 +1,12 @@
 const { execSync } = require('child_process');
 const ganacheCliInstalled = require('../node_modules/ganache-cli/package.json');
+const ganacheVersionRequired = require('../package.json').devDependencies['ganache-cli'];
 
 const baseOptions = { stdio: 'inherit' };
-const ganacheVersionRequired = '6.11.0-tezos.0';
 
 (function () {
-    console.log("ganache installed is", ganacheCliInstalled.version)
     if (ganacheCliInstalled.version !== ganacheVersionRequired) {
-        console.log('Updating ganache-cli')
-        execSync(`npm i ganache-cli@${ganacheVersionRequired}`, baseOptions)  
+        console.log('Updating ganache-cli to ', ganacheVersionRequired);
+        execSync(`npm i ganache-cli@${ganacheVersionRequired}`, baseOptions);
     };  
 })();
